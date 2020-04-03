8fe0042df2570d80403e7e51021991c2d52f2cd9
masrlinu <masrlinu@users.noreply.github.com>
2020-04-03 22:52:59 +0200

    test

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script>
+	import {testLoadedTwice} from 'relativepath/testLoadedTwice.js';
+	import Something from './Something.svelte';
+</script>
+
+<Something />
---
+++ src/Something.svelte
@@ -0,0 +1,5 @@
+<script>
+    import {testLoadedTwice} from './absolutepath/testLoadedTwice.js';
+</script>
+
+<div>Hallo!</div>
